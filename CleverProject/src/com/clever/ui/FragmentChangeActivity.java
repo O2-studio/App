6c3454988260a9471846776ebdb4d509f303118f
Xiaokun <xiaokunhou@gmail.com>
2014-06-28 15:16:20 +0800

    Add pull to refresh feature.

--- CleverProject/src/com/clever/ui/FragmentChangeActivity.java
+++ CleverProject/src/com/clever/ui/FragmentChangeActivity.java
@@ -8,52 +8,48 @@ import com.clever.ui.fragments.RightContentF;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 public class FragmentChangeActivity extends BaseActivity {
-	
-	private Fragment mContent;
-	
+
+	private Fragment rightPanel;
+
 	public FragmentChangeActivity() {
 		super(R.string.changing_fragments);
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// set the Above View
 		if (savedInstanceState != null)
-			mContent = getSupportFragmentManager().getFragment(savedInstanceState, "mContent");
-		if (mContent == null)
-			mContent = new RightContentF(R.color.red);	
-		
+			rightPanel = getSupportFragmentManager().getFragment(
+					savedInstanceState, "rightPanel");
+		if (rightPanel == null)
+			rightPanel = new RightContentF(R.color.red);
+
 		// set the Above View
 		setContentView(R.layout.content_frame);
-		getSupportFragmentManager()
-		.beginTransaction()
-		.replace(R.id.content_frame, mContent)
-		.commit();
-		
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.content_frame, rightPanel).commit();
+
 		// set the Behind View
 		setBehindContentView(R.layout.menu_frame);
-		getSupportFragmentManager()
-		.beginTransaction()
-		.replace(R.id.menu_frame, new LeftMenuF())
-		.commit();
-		
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.menu_frame, new LeftMenuF()).commit();
+
 		// customize the SlidingMenu
 		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
 	}
-	
+
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
-		getSupportFragmentManager().putFragment(outState, "mContent", mContent);
+		getSupportFragmentManager().putFragment(outState, "rightPanel",
+				rightPanel);
 	}
-	
+
 	public void switchContent(Fragment fragment) {
-		mContent = fragment;
-		getSupportFragmentManager()
-		.beginTransaction()
-		.replace(R.id.content_frame, fragment)
-		.commit();
+		rightPanel = fragment;
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.content_frame, fragment).commit();
 		getSlidingMenu().showContent();
 	}
 
--- CleverProject/src/com/clever/ui/fragments/RightContentF.java
+++ CleverProject/src/com/clever/ui/fragments/RightContentF.java
@@ -1,42 +1,97 @@
 package com.clever.ui.fragments;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.ListView;
 
+import com.clever.module.Doc;
 import com.clever.ui.R;
+import com.clever.ui.customized.ArticleViewAdapter;
+
 //Color panels on right side
-public class RightContentF extends Fragment {
-	
+@SuppressLint("ValidFragment")
+public class RightContentF extends Fragment implements
+		SwipeRefreshLayout.OnRefreshListener {
 	private int mColorRes = -1;
-	
-	public RightContentF() { 
-		this(R.color.white);
-	}
-	
+	private SwipeRefreshLayout swipeLayout;
+
+	// ListView to contain articles.
+	private ListView listView;
+	// ListView adapter
+	private ArticleViewAdapter adapter;
+	// All articles
+	private List<Doc> contentList;
+
 	public RightContentF(int colorRes) {
 		mColorRes = colorRes;
 		setRetainInstance(true);
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.articles, container, false);
+		swipeLayout = (SwipeRefreshLayout) view
+				.findViewById(R.id.swipe_refresh);
+		swipeLayout.setOnRefreshListener(this);
+
+		//top refresh style
+		swipeLayout.setColorScheme(android.R.color.holo_red_light,
+				android.R.color.holo_green_light,
+				android.R.color.holo_blue_bright,
+				android.R.color.holo_orange_light);
+
+		contentList = new ArrayList<Doc>();
+		Doc info = new Doc();
+		info.setContent("a long story");
+		contentList.add(info);
+		listView = (ListView) view.findViewById(R.id.listview);
+		adapter = new ArticleViewAdapter(getActivity(), contentList);
+		listView.setAdapter(adapter);
+
+		// set background color
 		if (savedInstanceState != null)
 			mColorRes = savedInstanceState.getInt("mColorRes");
 		int color = getResources().getColor(mColorRes);
-		// construct the RelativeLayout
-		RelativeLayout v = new RelativeLayout(getActivity());
-		v.setBackgroundColor(color);		
-		return v;
+		view.setBackgroundColor(color);
+
+		return view;
 	}
-	
+
 	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		outState.putInt("mColorRes", mColorRes);
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
 	}
-	
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+	}
+
+	public void onRefresh() {
+		new Handler().postDelayed(new Runnable() {
+			public void run() {
+				swipeLayout.setRefreshing(false);
+				Doc info = new Doc();
+				info.setContent("another knowledge");
+				contentList.add(info);
+				adapter.notifyDataSetChanged();
+			}
+		}, 500);
+	}
 }
